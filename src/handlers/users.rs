629d77434c30e1c0eef2b4d06460b3523f958499
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-05-24 00:54:33 +0900

    Add create_users API

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -1,6 +1,8 @@
 use crate::entities::prelude::User;
-use actix_web::{web, HttpResponse};
-use sea_orm::{DatabaseConnection, EntityTrait};
+use crate::entities::user::{self};
+use actix_web::{web, HttpResponse, Responder};
+use sea_orm::{DatabaseConnection, EntityTrait, Set};
+use serde::Deserialize;
 
 pub async fn get_users(db: web::Data<DatabaseConnection>) -> HttpResponse {
     let users = User::find()
@@ -8,4 +10,30 @@ pub async fn get_users(db: web::Data<DatabaseConnection>) -> HttpResponse {
         .await
         .expect("Failed to fetch users");
     HttpResponse::Ok().json(users)
+}
+
+#[derive(Deserialize)]
+pub struct CreateUserRequest {
+    pub name: String,
+    pub email: String,
+}
+
+pub async fn create_user(
+    db: web::Data<DatabaseConnection>,
+    request: web::Json<CreateUserRequest>,
+) -> impl Responder {
+    let request = request.into_inner();
+    let new_user = user::ActiveModel {
+        name: Set(request.name),
+        email: Set(request.email),
+        created_at: Set(chrono::Utc::now().into()),
+        updated_at: Set(chrono::Utc::now().into()),
+        ..Default::default()
+    };
+
+    let res = User::insert(new_user).exec(db.get_ref()).await;
+    match res {
+        Ok(_) => HttpResponse::Ok().body("User created"),
+        Err(_) => HttpResponse::InternalServerError().body("Failed to create user"),
+    }
 }
--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,4 +1,4 @@
-use crate::handlers::users::get_users;
+use crate::handlers::users::{create_user, get_users};
 use actix_web::{web, HttpResponse, Responder};
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
@@ -7,8 +7,4 @@ pub fn init_routes(cfg: &mut web::ServiceConfig) {
             .route(web::post().to(create_user))
             .route(web::get().to(get_users)),
     );
-}
-
-async fn create_user() -> impl Responder {
-    HttpResponse::Ok().body("Create user")
 }
