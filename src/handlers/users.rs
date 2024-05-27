c26f0a9ff4e17fcfb1d4850ba4ddb3487b789168
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-05-27 19:08:29 +0900

    Update create

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -2,7 +2,7 @@ use crate::entities::prelude::User;
 use crate::entities::user::{self};
 use crate::errors::ApiError;
 use actix_web::{web, HttpResponse, Responder};
-use sea_orm::{DatabaseConnection, EntityTrait, Set};
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Set};
 use serde::Deserialize;
 
 pub async fn get_users(db: web::Data<DatabaseConnection>) -> HttpResponse {
@@ -26,21 +26,37 @@ pub async fn create_user(
     request: web::Json<CreateUserRequest>,
 ) -> impl Responder {
     let request = request.into_inner();
-    let new_user = user::ActiveModel {
-        name: Set(request.name),
-        email: Set(request.email),
-        provider_id: Set(request.provider_id),
-        uid: Set(request.uid),
-        created_at: Set(chrono::Utc::now().into()),
-        updated_at: Set(chrono::Utc::now().into()),
-        ..Default::default()
-    };
 
-    let res = User::insert(new_user).exec(db.get_ref()).await;
-    match res {
-        Ok(_) => Ok(HttpResponse::Ok().body("User created")),
+    // Check if user with the same uid already exists
+    let existing_user = User::find()
+        .filter(user::Column::Uid.eq(&request.uid))
+        .one(db.get_ref())
+        .await;
+
+    match existing_user {
+        Ok(Some(_)) => Ok(HttpResponse::Conflict().body("User already exists")),
+        Ok(None) => {
+            let new_user = user::ActiveModel {
+                name: Set(request.name),
+                email: Set(request.email),
+                provider_id: Set(request.provider_id),
+                uid: Set(request.uid),
+                created_at: Set(chrono::Utc::now().into()),
+                updated_at: Set(chrono::Utc::now().into()),
+                ..Default::default()
+            };
+
+            let res = User::insert(new_user).exec(db.get_ref()).await;
+            match res {
+                Ok(_) => Ok(HttpResponse::Ok().body("User created")),
+                Err(e) => {
+                    log::error!("Failed to create user: {:?}", e);
+                    Err(ApiError::InternalServerError)
+                }
+            }
+        }
         Err(e) => {
-            log::error!("Failed to create user: {:?}", e);
+            log::error!("Failed to check existing user: {:?}", e);
             Err(ApiError::InternalServerError)
         }
     }
